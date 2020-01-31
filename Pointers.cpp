9fedcad038bebd0cde3a2e719ab782205eb056de
aviavinashkr <37958271+aviavinashkr@users.noreply.github.com>
2020-01-31 22:05:10 +0530

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int *ptr;      //creating a pointer named ptr
+  int num = 4;    //intilizaing an int type variable named num with value 4
+  cout << "The integer input is: " << num <<endl;
+  ptr = new int[num];  //using new to initialize the ptr array with size num and dynamically allocated memory in heap
+  cout << "Input " << num << " integers\n";
+  for (int i = 0; i < num; i++){
+    ptr[i] = i+1;   //assigning value i+1 to the index i of ptr array
+    cin >> ptr[i];  // we cin this value 
+  }
+  cout << "Elements entered by you are\n";
+  for (int i = 0; i < num; i++){
+    cout << ptr[i] << endl;    //displaying the values that we passed through cin above
+  }
+  delete[] ptr;      // now that we are done using ptr, we delete the array 
+                   // and free the part of memory which was allocated to it in the heap
+  return 0;
+}
